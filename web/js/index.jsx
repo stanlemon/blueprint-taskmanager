f62d5d55064d1d0e29477db815c992ccf0be63d4
Stan Lemon <stosh1985@gmail.com>
2015-10-18 21:59:01 -0400

    Add redux-thunk

--- web/js/index.jsx
+++ web/js/index.jsx
@@ -8,21 +8,18 @@ import Css from '../css/main.less';
 import App from './components/App';
 import TaskListView from './components/TaskListView';
 import TaskView from './components/TaskView';
-
 import { compose, createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { devTools, persistState } from 'redux-devtools';
 import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
-import tasksApp from './reducers';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
 
 const store = compose(
-  // Enables your middleware:
-  //applyMiddleware(m1, m2, m3), // any Redux middleware, e.g. redux-thunk
-  // Provides support for DevTools:
-  devTools(),
-  // Lets you write ?debug_session=<name> in address bar to persist debug sessions
-  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
-)(createStore)(tasksApp);
+    applyMiddleware(thunk),
+    devTools(),
+    persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore)(reducer);
 
 ReactDOM.render(
     <div>
