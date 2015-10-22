3731ee34f5453bb298d580d3b9e275a175dac86c
Stan Lemon <stosh1985@gmail.com>
2015-10-21 23:22:30 -0400

    More config cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.jsx?$/,
-                loaders: ['react-hot', 'babel'],
+                loaders: ['babel'],
                 include: path.join(__dirname, 'web/js')
             },
             {
