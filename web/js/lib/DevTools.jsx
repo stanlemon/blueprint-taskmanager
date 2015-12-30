d53f175390499ae61fe059e36d34439b227ebcf2
Stan Lemon <stosh1985@gmail.com>
2015-12-30 00:02:20 -0500

    Fix keys for dev tools

--- web/js/lib/DevTools.jsx
+++ web/js/lib/DevTools.jsx
@@ -4,7 +4,7 @@ import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
 export default createDevTools(
-    <DockMonitor defaultIsVisible={false} toggleVisibilityKey='H' changePositionKey='Q'>
+    <DockMonitor defaultIsVisible={false} toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q'>
         <LogMonitor/>
     </DockMonitor>
 );
