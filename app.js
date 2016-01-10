8a0cce38de90f4ef30caf68332bec9d097213528
Stan Lemon <stosh1985@gmail.com>
2016-01-09 22:57:42 -0500

    Don't force a db reset

--- app.js
+++ app.js
@@ -198,7 +198,7 @@ resources.Task.create.write.before((req, res, context) => {
 });
 
 
-db.sequelize.sync({ force: true })
+db.sequelize.sync(/*{ force: true }*/)
     .then( () => {
         let server = http.createServer(app);
 
