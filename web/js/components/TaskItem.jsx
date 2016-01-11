a9c10aa253c2c74104393e11daa60bc0108364ca
Stan Lemon <stosh1985@gmail.com>
2016-01-10 22:22:07 -0500

    Cleaned up use of state and change the way a line through is applied to the task name

--- web/js/components/TaskItem.jsx
+++ web/js/components/TaskItem.jsx
@@ -7,12 +7,6 @@ import { Link } from 'react-router';
 
 export default class TaskItem extends React.Component {
 
-    static defaultProps = {
-        task: {
-            completed: null
-        }
-    };
-
     static propTypes = {
         task: React.PropTypes.shape({
             id: React.PropTypes.number,
@@ -22,14 +16,6 @@ export default class TaskItem extends React.Component {
             completed: React.PropTypes.string
         })
     };
-
-    constructor(props, context) {
-        super(props, context);
-
-        this.state = {
-            completed: this.props.task.completed,
-        };
-    }
 
     deleteTask() {
         this.props.actions.deleteTask(this.props.task.id);
@@ -41,7 +27,7 @@ export default class TaskItem extends React.Component {
 
     completeTask(t) {
         return {
-           value: t.completed !== null,
+           value: this.props.task.completed !== null,
            requestChange: (checked) => {
                this.props.actions.updateTask({
                    ...this.props.task,
@@ -51,28 +37,23 @@ export default class TaskItem extends React.Component {
         };
     }
 
-    componentWillReceiveProps(nextProps) {
-        if (!isEqual(this.props, nextProps)) {
-            this.setState({
-                completed: nextProps.task.completed
-            });
-        }
-    }
-
     render() {
-        let task = this.props.task;
-        let taskName = task.completed === null ? <span>{task.name}</span> : <s>{task.name}</s>;
+        const { task } = this.props;
+
+        const styles = {
+            textDecoration: task.completed ? 'line-through' : 'none'
+        }
 
         return (
             <div style={{ border: '1px solid #e3e3e3', borderRadius: '4px', marginBottom: '8px' }}>
                 <div className="row" style={{ margin: '10px' }}>
-                    <div className="col-xs-7 col-sm-9 col-md-10" onClick={this.viewTask.bind(this)}>
-                        {taskName}
+                    <div style={styles} className="col-xs-7 col-sm-9 col-md-10" onClick={this.viewTask.bind(this)}>
+                        {task.name}
                     </div>
                     <div className="col-xs-5 col-sm-3 col-md-2">
                         <div className="row">
                             <div className="col-xs-5 col-sm-7 text-right">
-                                <input type="checkbox" checkedLink={this.completeTask(task)} />
+                                <input type="checkbox" checkedLink={this.completeTask()} />
                             </div>
                             <div className="col-xs-7 col-sm-3 text-right">
                                 <button type="button" className="btn btn-xs btn-danger" onClick={this.deleteTask.bind(this)}>Delete</button>
