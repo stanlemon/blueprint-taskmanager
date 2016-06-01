c8c4a005da6c3f65ecb8997557b8c3ac3f0a2c8f
Stan Lemon <stosh1985@gmail.com>
2016-05-31 21:50:55 -0400

    Try this test by stripping off the timezone.

--- web/test/lib/UtilsTest.js
+++ web/test/lib/UtilsTest.js
@@ -3,37 +3,26 @@
 import { assert } from 'chai';
 import { makeDateTime } from '../../js/lib/Utils';
 
-const tz = (d) => d.toString().substr(-11, 6).trim();
-
 describe('Utils', () => {
     it('makeDateTime formats mysql friendly dates', () => {
-
-        const dt1 = new Date('June 13, 1985 08:00:00');
-
         assert.equal(
-            makeDateTime(dt1),
-            '1985-06-13 13:00:00.000 ' + tz(dt1)
+            makeDateTime(new Date('June 13, 1985 08:00:00')).substring(0, 23),
+            '1985-06-13 13:00:00.000'
         );
 
-        const dt2 = new Date('November 6, 2009 05:00:00');
-
         assert.equal(
-            makeDateTime(dt2),
-            '2009-11-06 10:00:00.000 ' + tz(dt2)
+            makeDateTime(new Date('November 6, 2009 05:00:00')).substring(0, 23),
+            '2009-11-06 10:00:00.000'
         );
 
-        const dt3 = new Date('December 12, 2011 05:00:00');
-
         assert.equal(
-            makeDateTime(dt3),
-            '2011-12-12 10:00:00.000 ' + tz(dt2)
+            makeDateTime(new Date('December 12, 2011 05:00:00')).substring(0, 23),
+            '2011-12-12 10:00:00.000'
         );
 
-        const dt4 = new Date('June 2, 2014 07:30:00');
-
         assert.equal(
-            makeDateTime(dt4),
-            '2014-06-02 11:30:00.000 ' + tz(dt4)
+            makeDateTime(new Date('June 2, 2014 07:30:00')).substring(0, 23),
+            '2014-06-02 11:30:00.000'
         );
     });
 });
