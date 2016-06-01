df99331960db234b1e1c88f0df5b5f2eff47b87b
Stan Lemon <stosh1985@gmail.com>
2016-05-31 21:59:37 -0400

    One more try on the make date tests

--- web/js/lib/Utils.js
+++ web/js/lib/Utils.js
@@ -1,9 +1,10 @@
-/*eslint-disable */
+import moment from 'moment';
+
 export function makeDateTime(d = new Date()) {
-    return d.toISOString().replace('T', ' ').replace('Z', '')
-        + ' ' + d.toString().substr(-11, 6).trim();
+    return moment(d).format('YYYY-MM-DD HH:mm:ss.SSS z');
 }
 
+/*eslint-disable */
 export function mapErrors(errors = []) {
     return errors.reduce((o, v) => {
         o[v.field] = [v.message];
--- web/test/lib/UtilsTest.js
+++ web/test/lib/UtilsTest.js
@@ -6,22 +6,22 @@ import { makeDateTime } from '../../js/lib/Utils';
 describe('Utils', () => {
     it('makeDateTime formats mysql friendly dates', () => {
         assert.equal(
-            makeDateTime(new Date('June 13, 1985 08:00:00')).substring(0, 23),
+            makeDateTime(new Date('June 13, 1985 13:00:00')).substring(0, 23),
             '1985-06-13 13:00:00.000'
         );
 
         assert.equal(
             makeDateTime(new Date('November 6, 2009 05:00:00')).substring(0, 23),
-            '2009-11-06 10:00:00.000'
+            '2009-11-06 05:00:00.000'
         );
 
         assert.equal(
-            makeDateTime(new Date('December 12, 2011 05:00:00')).substring(0, 23),
-            '2011-12-12 10:00:00.000'
+            makeDateTime(new Date('December 12, 2011 22:00:00')).substring(0, 23),
+            '2011-12-12 22:00:00.000'
         );
 
         assert.equal(
-            makeDateTime(new Date('June 2, 2014 07:30:00')).substring(0, 23),
+            makeDateTime(new Date('June 2, 2014 11:30:00')).substring(0, 23),
             '2014-06-02 11:30:00.000'
         );
     });
