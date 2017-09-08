eb4d57ff485399b51698de9ecbd49af3712332e9
Stan Lemon <stosh1985@gmail.com>
2017-09-08 17:09:11 -0400

    More bind() cleanup

--- web/js/components/Routes.js
+++ web/js/components/Routes.js
@@ -1,92 +1,63 @@
 import React from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import SessionWatcher from './SessionWatcher';
 import Layout from './Layout';
 import LoginView from './LoginView';
 import RegisterView from './RegisterView';
 import TaskListView from './TaskListView';
 import TaskView from './TaskView';
+import createHashHistory from 'history/createHashHistory';
 
-// Wraps react-router to future proof against API changes
+const history = createHashHistory();
+
 function navigateTo(route) {
-    this.history.push(route);
+    history.push(route);
 }
 
 export default function Routes(props) {
     return (
-        <Router>
-            <Route
-                path="/"
-                render={router => (
-                    <SessionWatcher
-                        {...props}
-                        path={router.location.pathname}
-                        navigateTo={navigateTo.bind(router)}
-                    >
-                        <Switch>
-                            <Route
-                                path="/login"
-                                render={router => (
-                                    <LoginView
+        <Router history={history}>
+            <SessionWatcher
+                {...props}
+                path={history.location.pathname}
+                navigateTo={navigateTo}
+            >
+                <Switch>
+                    <Route path="/login">
+                        <LoginView {...props} navigateTo={navigateTo} />
+                    </Route>
+                    <Route path="/register">
+                        <RegisterView {...props} navigateTo={navigateTo} />
+                        )}
+                    </Route>
+                    <Route path="/">
+                        <Layout {...props} navigateTo={navigateTo}>
+                            <Switch>
+                                <Route exact path="/">
+                                    <TaskListView
                                         {...props}
-                                        navigateTo={navigateTo.bind(router)}
+                                        navigateTo={navigateTo}
                                     />
-                                )}
-                            />
-                            <Route
-                                path="/register"
-                                render={router => (
-                                    <RegisterView
-                                        {...props}
-                                        navigateTo={navigateTo.bind(router)}
-                                    />
-                                )}
-                            />
-                            <Route
-                                path="/"
-                                render={router => (
-                                    <Layout
-                                        {...props}
-                                        navigateTo={navigateTo.bind(router)}
-                                    >
-                                        <Switch>
-                                            <Route
-                                                exact
-                                                path="/"
-                                                render={router => (
-                                                    <TaskListView
-                                                        {...props}
-                                                        navigateTo={navigateTo.bind(
-                                                            router
-                                                        )}
-                                                    />
-                                                )}
-                                            />
-                                            <Route
-                                                exact
-                                                path="/view/:id"
-                                                render={router => (
-                                                    <TaskView
-                                                        {...props}
-                                                        navigateTo={navigateTo.bind(
-                                                            router
-                                                        )}
-                                                        taskId={parseInt(
-                                                            router.match.params
-                                                                .id,
-                                                            10
-                                                        )}
-                                                    />
-                                                )}
-                                            />
-                                        </Switch>
-                                    </Layout>
-                                )}
-                            />
-                        </Switch>
-                    </SessionWatcher>
-                )}
-            />
+                                </Route>
+                                <Route
+                                    exact
+                                    path="/view/:id"
+                                    render={router => (
+                                        <TaskView
+                                            {...props}
+                                            navigateTo={navigateTo}
+                                            taskId={parseInt(
+                                                router.match.params.id,
+                                                10
+                                            )}
+                                        />
+                                    )}
+                                />
+                            </Switch>
+                        </Layout>
+                    </Route>
+                </Switch>
+            </SessionWatcher>
         </Router>
     );
 }
