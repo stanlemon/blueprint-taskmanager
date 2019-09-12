f9f957746263665ce1df0447839c2b7ba07faa61
Stan Lemon <stanlemon@users.noreply.github.com>
2019-09-11 23:17:33 -0400

    Another fix for hmr

--- web/js/index.js
+++ web/js/index.js
@@ -1,12 +1,8 @@
 import "whatwg-fetch";
 import "react-hot-loader";
+import { hot } from "react-hot-loader";
 import React from "react";
 import { render } from "react-dom";
 import App from "./components/App";
 
 render(<App />, document.getElementById("root"));
-
-// Only used in dev for hot-reloading
-if (module.hot) {
-  module.hot.accept();
-}
