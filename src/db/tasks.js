3a86394a52c584de4012083a797a218d3472dfb1
Stan Lemon <stanlemon@users.noreply.github.com>
2019-09-17 21:21:31 -0400

    Database methods for future use

--- src/db/tasks.js
+++ src/db/tasks.js
@@ -1,4 +1,6 @@
 const knex = require("../connection");
+const omit = require("lodash/omit");
+const format = require("date-fns/format");
 
 const columns = [
   "id",
@@ -24,4 +26,52 @@ function getTaskById(userId, taskId) {
     .first();
 }
 
-module.exports = { getTasks, getTaskById };
+function deleteTaskById(userId, taskId) {
+  return knex("tasks")
+    .where("user_id", userId)
+    .where("id", taskId)
+    .delete();
+}
+
+function updateTask(userId, taskId, task) {
+  return getTaskById(userId, taskId).then(verify => {
+    if (verify.userId !== userId) {
+      throw Error("Task does not belong to user");
+    }
+
+    const data = Object.assign(
+      {},
+      // Exclude the following properties
+      omit(task, ["created_at", "updated_at", "user_id"]),
+      {
+        userId,
+        updated_at: format(Date.now(), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx"),
+      }
+    );
+
+    return knex("tasks")
+      .where({ id: taskId })
+      .update(data);
+  });
+}
+
+function createTask(userId, task) {
+  // Make a sql friendly date for now
+  const now = format(Date.now(), "yyyy-MM-dd'T'HH:mm:ss.SSSxxx");
+  // Build a copy of the input with our required fields
+  const data = Object.assign({}, data, {
+    userId,
+    created_at: now,
+    updated_at: now,
+  });
+
+  return knex("tasks").insert(task);
+}
+
+module.exports = {
+  getTasks,
+  getTaskById,
+  deleteTaskById,
+  updateTask,
+  createTask,
+};
