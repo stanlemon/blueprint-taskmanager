a1694eaec20fbd2a835173958f9c83731e121035
Stan Lemon <stanlemon@users.noreply.github.com>
2019-11-23 09:19:42 -0500

    Split the App component off from react so we have smaller files

--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,8 @@
 import "react-hot-loader";
 import React from "react";
 import { render } from "react-dom";
-import App from "./components/App";
+import loadable from "@loadable/component";
+
+const App = loadable(() => import("./components/App"));
 
 render(<App />, document.getElementById("root"));
