ba9903a912c955a749cb38b6ee0abe7f19b6f062
Stan Lemon <stanlemon@users.noreply.github.com>
2020-11-13 20:26:06 -0500

    Update LoginView test to use testing-library

--- web/js/components/LoginView.test.js
+++ web/js/components/LoginView.test.js
@@ -1,39 +1,29 @@
 import React from "react";
-import { mount, configure } from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import { fireEvent, render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import { history } from "../lib/Navigation";
 import { LoginView } from "./LoginView";
-import Error from "./Error";
 import { ROUTE_REGISTER } from "./Routes";
-
-configure({ adapter: new Adapter() });
 
 describe("<LoginView />", () => {
   it("should render a login screen with empty fields", () => {
-    const view = mount(<LoginView login={() => {}} clearErrors={() => {}} />);
+    render(<LoginView login={() => {}} clearErrors={() => {}} />);
 
-    const username = view.find('input[name="username"]');
-
-    expect(username.props().value).toEqual("");
-
-    const password = view.find('input[name="password"]');
-
-    expect(password.props().value).toEqual("");
+    expect(screen.getByLabelText("Email")).toHaveValue("");
+    expect(screen.getByLabelText("Password")).toHaveValue("");
   });
 
   it("should error when fields are submitted blank", () => {
-    const view = mount(<LoginView login={() => {}} clearErrors={() => {}} />);
+    render(<LoginView login={() => {}} clearErrors={() => {}} />);
 
-    view.find("form").simulate("submit");
+    fireEvent.click(screen.getByRole("button", { name: "Login" }));
 
-    view.update();
-
-    const errors = view.find(".error");
-
-    expect(errors.length).toBe(2);
-
-    expect(errors.at(0).text()).toBe("You must enter your username.");
-    expect(errors.at(1).text()).toBe("You must enter your password.");
+    expect(
+      screen.getByText("You must enter your username.")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("You must enter your password.")
+    ).toBeInTheDocument();
   });
 
   it("should submit a username and password", () => {
@@ -42,50 +32,57 @@ describe("<LoginView />", () => {
       username = data.username;
       password = data.password;
     };
-    const view = mount(<LoginView login={login} clearErrors={() => {}} />);
+
+    render(<LoginView login={login} clearErrors={() => {}} />);
 
     const expectedUsername = "test@test.com";
     const expectedPassword = "p@$$w0rd";
 
-    const usernameInput = view.find('input[name="username"]');
-    usernameInput.simulate("change", {
-      target: { name: "username", value: expectedUsername },
+    const usernameField = screen.getByLabelText("Email");
+
+    fireEvent.change(usernameField, {
+      target: {
+        value: expectedUsername,
+      },
     });
 
-    const passwordInput = view.find('input[name="password"]');
-    passwordInput.simulate("change", {
-      target: { name: "password", value: expectedPassword },
+    expect(usernameField).toHaveValue(expectedUsername);
+
+    const passwordField = screen.getByLabelText("Password");
+
+    fireEvent.change(passwordField, {
+      target: {
+        value: expectedPassword,
+      },
     });
 
-    view.find("form").simulate("submit");
+    expect(passwordField).toHaveValue(expectedPassword);
 
-    expect(view.find(".error").length).toBe(0);
+    fireEvent.click(screen.getByRole("button", { name: "Login" }));
 
     expect(username).toBe(expectedUsername);
     expect(password).toBe(expectedPassword);
   });
 
   it("clicking the button to go to the register screen should trigger navigateTo", () => {
-    const view = mount(<LoginView login={() => {}} clearErrors={() => {}} />);
+    render(<LoginView login={() => {}} clearErrors={() => {}} />);
 
-    const button = view.findWhere(
-      (n) => n.type() === "a" && n.text() === "Create Account"
-    );
-
-    button.simulate("click");
+    fireEvent.click(screen.getByText("Create Account"));
 
     expect(history.location.pathname).toBe(ROUTE_REGISTER);
   });
 
   it("errors from actions render", () => {
-    const view = mount(
+    const errorMessage = "Error message " + Date.now();
+
+    render(
       <LoginView
         login={() => {}}
-        errors={{ main: "Error message" }}
+        errors={{ main: errorMessage }}
         clearErrors={() => {}}
       />
     );
 
-    expect(view.find(Error).length).toBe(1);
+    expect(screen.getByText(errorMessage)).toBeInTheDocument();
   });
 });
