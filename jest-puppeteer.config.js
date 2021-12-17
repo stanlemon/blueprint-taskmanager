8db164cca8098f88d8d05e5109d5033cb60cbc94
Stan Lemon <stanlemon@users.noreply.github.com>
2021-12-16 20:35:06 -0500

    Bump puppet server launch time

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,7 +3,7 @@ module.exports = {
     headless: true,
   },
   server: {
-    launchTimeout: 10000,
+    launchTimeout: 30000,
     command: "npm run build; PORT=19292 NODE_ENV=test npm start",
     port: 19292,
   },
