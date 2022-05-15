b4c8fc2c6abf395281fa66538c60d9a44e058dc7
Stan Lemon <86314+stanlemon@users.noreply.github.com>
2022-05-14 20:23:38 -0400

    Dependency updates, fix jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,9 +1,6 @@
 import config from "@stanlemon/webdev/jest.config.js";
 
 config.preset = "jest-puppeteer";
-config.moduleNameMapper = {
-  "wouter/matcher": "wouter/cjs/matcher",
-};
 config.collectCoverageFrom = ["src/**/*.{js,jsx}", "web/js/**/*.{js,jsx}"];
 config.coveragePathIgnorePatterns = [
   "/node_modules/",
